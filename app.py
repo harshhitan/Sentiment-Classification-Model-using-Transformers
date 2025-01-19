11036de54ede43c9939c84981baf76ffb3a0b8a1
harshhitan <harshitanand243@gmail.com>
2025-01-19 22:40:49 +0530

    Files

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from transformers import pipeline
+
+st.title("Fine-Tuning BERT for IMDB Review Classification")
+
+classifier = pipeline('text-classification', model='distilbert-base-uncased-sentiment-model')
+
+text = st.text_area("Enter Your Review Here")
+text = text[:512]
+
+if st.button("Predict"):
+        result = classifier(text)
+        st.write("Prediction Result:", result)
