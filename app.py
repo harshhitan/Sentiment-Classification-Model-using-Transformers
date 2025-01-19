8cd053d76cd4030fd9a504dfd1b7caf2ef4e1951
harshhitan <harshitanand243@gmail.com>
2025-01-19 23:09:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 
 st.title("Fine-Tuning BERT for IMDB Review Classification")
 
-classifier = pipeline('text-classification', model='distilbert-base-uncased-sentiment-model')
+classifier = pipeline('text-classification', model='harshhitan/distilbert-base-uncased-sentiment-model')
 
 text = st.text_area("Enter Your Review Here")
 text = text[:512]
